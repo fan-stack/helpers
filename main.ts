fd6f903e48f53f19c599cd482b2b9134c1d0e390
Emerson Jair <emerson.jair@forrestbin.com>
2019-03-10 20:29:18 -0300

    feat(decorator): add decorator to use with NestJS modules
    add another function to return an HttpsFunction directly instead of returning only the handler
    
    close #15

--- main.ts
+++ main.ts
@@ -1,10 +1,59 @@
+// tslint:disable: jsdoc-format
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
+import { https } from 'firebase-functions';
 
+/**
+ * Returns an HTTP handler that can be used on Firebase HTTPS Functions.
+ *
+ * @example
+ * export const api = functions.https.onRequest(nestToFirebase(AppModule))
+ * @deprecated Use `nestToFirebaseHandler` instead
+ * @param nestModule NestJS module
+ */
 export function nestToFirebase(nestModule: any) {
   return async (req: express.Request, res: express.Response) => {
     const expressInstance = express();
     await (await NestFactory.create(nestModule, expressInstance)).init();
     expressInstance(req, res);
   };
+}
+
+/**
+ * Returns an HTTP handler that can be used on Firebase HTTPS Functions.
+ *
+ * @example
+ * export const api = functions.https.onRequest(nestToFirebaseHandler(AppModule))
+ * @param nestModule NestJS module
+ */
+export function nestToFirebaseHandler(nestModule: any) {
+  return nestToFirebase(nestModule);
+}
+
+/**
+ * Returns an HTTP Function ready to be exported.
+ *
+ * @example
+ * export const api = nestToFirebaseFunction(AppModule)
+ * @param nestModule NestJS module
+ */
+export function nestToFirebaseFunction(nestModule: any) {
+  return https.onRequest(nestToFirebaseHandler(nestModule));
+}
+
+/**
+ * Transforms a NestJS Module into an HTTPS Firebase Function.
+ *
+ * @example
+ * ```ts
+@FanHttp()
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService]
+})
+export class UsersModule {}
+  ```
+ */
+export function FanHttp(): ClassDecorator {
+  return (target: object) => nestToFirebaseFunction(target) as any;
 }
